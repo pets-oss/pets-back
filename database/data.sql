c94b51926ea02f6831e013746618493ca46b1e4d
AzisK <akrusna@kayak.com>
2020-12-05 19:28:21 +0200

    #12 Create DB tables according to class diagram
    Added tables with sample data according to ER diagram. Few notes:
    
    a) I could not go with table name "user" because this is a reserved word in postgresql. Now it's a little weird that all tables are in singular except one is in plural. Should we rename users or go with plural names for tables?
    b) Few-word column naming is different. Sometimes it's snake_case (full_name, registration_no etc.) and sometimes it's camelCase (dateTime, imageUrl etc.). I believe we should unify the style.
    c) From the diagram I did not understand the data type of eventType. Should it be an enum? For now I just made it a VARCHAR to be more flexible since we do not know all the possible event types and they might change in the future.
    d) We might need a time-zone aware timestamp but I am not sure about it at the moment
    e) I believe we should add a "modtime" column to users, animal, organization tables since they might be modified and modtime would be good to track and help debug what happened
    f) We will ammend enum values later, now I do not know them. More about them:
    https://www.postgresql.org/docs/9.1/functions-enum.html
    https://dzone.com/articles/when-you-should-and-should-not

--- database/data.sql
+++ database/data.sql
@@ -1,1 +1,47 @@
-INSERT INTO test_table (test_var) VALUES ('FOO'), ('BAR');
+INSERT INTO organization (name, address, phone)
+VALUES 
+('Haskelis', 'Vytauto 1', '123'),
+('Jupyteris', 'Mindaugo 12', '+371'),
+('Javainis', 'Ulrich Strasse 99', '+80871'),
+('Pitoncas', 'Kalnų 38', '0044371');
+
+INSERT INTO users (id, username, full_name, email, organization, role_type)
+VALUES 
+('dhjbwau74a6', 'Svx', 'Sveikas', 'svx@svx.lt', 1, 'Owner'),
+('aiubfaw4io09', 'Hi', 'Hello', 'hi@hi.lt', 1, 'Member'),
+('afhu9w4f78', 'Carl', 'Red', 'hi@hi.lt', 2, 'Owner'),
+('278y2378ryb', 'Bob', 'Blue', 'hi@hi.lt', 2, 'Member'),
+('0932hfdsa', 'Mamba', 'Black', 'hi@hello.lt', 2, NULL);
+
+INSERT INTO animal (organization, registration_no, status, imageUrl, birthDate, name, species, gender, microchip_id, chip_install_date)
+VALUES 
+(1, '123Svx', 'vaccinated', 'www.url.lt', '2020-08-01', 'Haskelis', 'dog', 'female', '123', '2020-08-11'),
+(2, '321Hi', 'sick', 'www.url.lt', '2020-01-01', 'Jupyteris', 'parrot', 'female', '666666', '2020-09-01'),
+(3, '456Carl', 'healthy', 'www.url.lt', '2020-01-01', 'Pitoncas', 'snake', 'male', '2893402', '2020-03-01'),
+(3, '555Bob', 'adopted', 'www.url.lt', '2020-01-01', 'Javainis', 'hamster', 'male', '29387', '2020-04-14'),
+(1, '999Mamba', 'healthy', 'www.url.lt', '2019-09-16', 'Murkė', 'cat', 'male', '001010101', '2020-09-01');
+
+INSERT INTO animal_event_check_in (animal, dateTime, comments, organization)
+VALUES
+(1, '2020-03-01', 'Beautiful dog', 1),
+(2, '2020-03-01', 'Beautiful parrot', 1),
+(3, '2020-03-01', 'Beautiful snake', 1),
+(4, '2020-03-01', 'Beautiful hamster', 1),
+(5, '2020-04-01', 'Beautiful cat', 1),
+(1, '2020-03-02', 'Re-registering', 1),
+(2, '2020-03-02', 'Re-registering', 1),
+(3, '2020-03-02', 'Re-registering', 1),
+(4, '2020-03-02', 'Re-registering', 1),
+(5, '2020-04-02', 'Re-registering', 1);
+
+INSERT INTO animal_event_check_out (animal, dateTime, comments, organization)
+VALUES
+(5, '2020-10-01', 'Adopted cat', 1);
+
+INSERT INTO animal_event_general (animal, dateTime, comments, organization, "type", expenses)
+VALUES
+(2, '2020-07-01', 'Birthday party!', 1, 'birthday', 109.03);
+
+INSERT INTO animal_event_medical_record (animal, dateTime, comments, organization, "type", expenses)
+VALUES
+(4, '2020-05-21', 'Dewormed', 1, 'deworm', 26.74);
--- database/schema.sql
+++ database/schema.sql
@@ -1,3 +1,73 @@
-CREATE TABLE test_table (
-    test_var    char(255)
-); 
+CREATE TABLE organization (
+    id SMALLSERIAL PRIMARY KEY,
+    name VARCHAR(256) NOT NULL,
+    address VARCHAR(512),
+    phone VARCHAR(64)
+);
+
+CREATE TYPE role_type AS ENUM ('Owner', 'Member', 'Reader', 'Stranger');
+
+CREATE TABLE users (
+    id VARCHAR(256) PRIMARY KEY,
+    username VARCHAR(128) NOT NULL,
+    full_name VARCHAR(256),
+    email VARCHAR(128),
+    organization SMALLINT REFERENCES organization(id),
+    role_type role_type DEFAULT 'Stranger'
+);
+
+CREATE TYPE status AS ENUM ('healthy', 'vaccinated', 'sick', 'adopted');
+CREATE TYPE gender AS ENUM ('male', 'female');
+CREATE TYPE species AS ENUM ('cat', 'dog', 'hamster', 'parrot', 'snake');
+
+CREATE TABLE animal (
+    id SERIAL PRIMARY KEY,
+    organization SMALLINT REFERENCES organization(id),
+    registration_no VARCHAR(256) NOT NULL,
+    status status,
+    imageUrl VARCHAR(512),
+    birthDate DATE,
+    name VARCHAR(128),
+    species species,
+    gender gender,
+    microchip_id VARCHAR(256),
+    chip_install_date DATE,
+    food VARCHAR(256),
+    comments TEXT
+);
+
+CREATE TABLE animal_event_check_in (
+    id SERIAL PRIMARY KEY,
+    animal INTEGER REFERENCES animal(id),
+    dateTime TIMESTAMP,
+    comments TEXT,
+    organization SMALLINT REFERENCES organization(id)
+);
+
+CREATE TABLE animal_event_check_out (
+    id SERIAL PRIMARY KEY,
+    animal INTEGER REFERENCES animal(id),
+    dateTime TIMESTAMP,
+    comments TEXT,
+    organization SMALLINT REFERENCES organization(id)
+);
+
+CREATE TABLE animal_event_general (
+    id SERIAL PRIMARY KEY,
+    animal INTEGER REFERENCES animal(id),
+    dateTime TIMESTAMP,
+    comments TEXT,
+    organization SMALLINT REFERENCES organization(id),
+    type VARCHAR(64),
+    expenses NUMERIC
+);
+
+CREATE TABLE animal_event_medical_record (
+    id SERIAL PRIMARY KEY,
+    animal INTEGER REFERENCES animal(id),
+    dateTime TIMESTAMP,
+    comments TEXT,
+    organization SMALLINT REFERENCES organization(id),
+    type VARCHAR(64),
+    expenses NUMERIC
+);
