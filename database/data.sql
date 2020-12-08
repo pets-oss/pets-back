df21230ea327d45dfe12f2f113445c4ff5066d09
Azis <akrusna@kayak.com>
2020-12-08 10:30:16 +0200

    #12 Create DB tables according to class diagram (#17)
    * #12 Create DB tables according to class diagram
    
    Added tables with sample data according to ER diagram. Few notes:
    
    a) I could not go with table name "user" because this is a reserved word in postgresql. Now it's a little weird that all tables are in singular except one is in plural. Should we rename users or go with plural names for tables?
    b) Few-word column naming is different. Sometimes it's snake_case (full_name, registration_no etc.) and sometimes it's camelCase (dateTime, imageUrl etc.). I believe we should unify the style.
    c) From the diagram I did not understand the data type of eventType. Should it be an enum? For now I just made it a VARCHAR to be more flexible since we do not know all the possible event types and they might change in the future.
    d) We might need a time-zone aware timestamp but I am not sure about it at the moment
    e) I believe we should add a "modtime" column to users, animal, organization tables since they might be modified and modtime would be good to track and help debug what happened
    f) We will ammend enum values later, now I do not know them. More about them:
    https://www.postgresql.org/docs/9.1/functions-enum.html
    https://dzone.com/articles/when-you-should-and-should-not
    
    * Change users table to app_user
    
    * Change event type data-type from VARCHAR to ENUM
    
    Additionally separate event_general and event_medical ENUM since we also have different tables for them
    
    * Unify the naming style of DB columns to snake_case
    
    * Add modtime to organization, app_user, animal
    
    Make it auto-update according to https://dba.stackexchange.com/a/157096
    
    * Typo addoption -> adoption
    
    * Change role_type from Stranger to Guest
    
    * Change from Black Mamba to Green Mamba
    
    * Move moddatetime extension and table triggers to schema.sql
    
    Triggers automatically update modtime with any update of the table.
    
    It's part of the definition of the tables and their behavior. Thus, it should stay in schema.sql.
    
    * Remove redundant space
    
    * Separate full_name to 2 separate fields "name" and "surname"
    
    * Change organization id from SMALLSERIAL to SERIAL
    
    Also change the foreign keys referencing it to INTEGER from SMALLINT
    
    * Rename modtime to mod_time
    
    * Decompose address field into country, city, street, house and flat fields
    
    * Typo in SQL insert
    
    * Change Postgresql date-type TIMESTAMP to upper-case
    
    * Addition of distinctive fields to animal
    
    I believe these will be needed sooner or later. Data-types could be different.
    
    I think it's hard to put all the possible breeds of all the species, may be not that good for an ENUM as well but this could be done.
    
    I left allergy as a VARCHAR because this is also very undefined. Could be any allergy, could be a few.
    
    * Add missing "," in CREATE TABLE
    
    * Add values for newly added fields to animal table

--- database/data.sql
+++ database/data.sql
@@ -1,1 +1,53 @@
-INSERT INTO test_table (test_var) VALUES ('FOO'), ('BAR');
+INSERT INTO organization (name, country, city, street, house, flat, phone)
+VALUES 
+('Haskelis', 'Lietuva', 'Kaunas', 'Vytauto', '1a', NULL, '123'),
+('Jupyteris', 'Lietuva', 'Paryžius', 'Mindaugo', '12', '1', '+371'),
+('Javainis', 'Lietuva', 'Šveicarija', 'Ulrich Strasse', '99', NULL, '+80871'),
+('Pitoncas', 'Vokietija', 'Konstanca', 'Kalnų', '38', '5', '0044371');
+
+UPDATE organization SET phone = '1141184' WHERE id = 1;
+
+INSERT INTO app_user (id, username, name, surname, email, organization, role_type)
+VALUES 
+('dhjbwau74a6', 'Svx', 'Sveikas', 'Ūsas', 'svx@svx.lt', 1, 'Owner'),
+('aiubfaw4io09', 'Hi', 'Hello', 'Letena', 'hi@hi.lt', 1, 'Member'),
+('afhu9w4f78', 'Carl', 'Red', 'Gauras', 'hi@hi.lt', 2, 'Owner'),
+('278y2378ryb', 'Bob', 'Blue', 'Eyes', 'hi@hi.lt', 2, 'Member'),
+('0932hfdsa', 'Mamba', 'Green', 'Jungleen', 'hi@hello.lt', 2, NULL);
+
+UPDATE app_user SET email = 'green@mamba.lt' WHERE id = 'dhjbwau74a6';
+
+INSERT INTO animal (organization, registration_no, status, image_url, birth_date, name, species, breed, gender, color, weight, microchip_id, chip_install_date)
+VALUES
+(1, '123Svx', 'vaccinated', 'www.url.lt', '2020-08-01', 'Haskelis', 'dog', 'husky', 'female', 'white', 20, '123', '2020-08-11'),
+(2, '321Hi', 'sick', 'www.url.lt', '2020-01-01', 'Jupyteris', 'parrot', NULL, 'female', 'green', 1, '666666', '2020-09-01'),
+(3, '456Carl', 'healthy', 'www.url.lt', '2020-01-01', 'Pitoncas', 'snake', NULL, 'male', 'green', 0.3, '2893402', '2020-03-01'),
+(3, '555Bob', 'adopted', 'www.url.lt', '2020-01-01', 'Javainis', 'hamster', NULL, 'male', 'orange', 0.4, '29387', '2020-04-14'),
+(1, '999Mamba', 'healthy', 'www.url.lt', '2019-09-16', 'Murkė', 'cat', 'ragdoll', 'male', 'grey', 7, '001010101', '2020-09-01');
+
+UPDATE animal SET image_url = 'www.green.mamba' WHERE id = 1;
+
+INSERT INTO animal_event_check_in (animal, date_time, comments, organization)
+VALUES
+(1, '2020-03-01', 'Beautiful dog', 1),
+(2, '2020-03-01', 'Beautiful parrot', 1),
+(3, '2020-03-01', 'Beautiful snake', 1),
+(4, '2020-03-01', 'Beautiful hamster', 1),
+(5, '2020-04-01', 'Beautiful cat', 1),
+(1, '2020-03-02', 'Re-registering', 1),
+(2, '2020-03-02', 'Re-registering', 1),
+(3, '2020-03-02', 'Re-registering', 1),
+(4, '2020-03-02', 'Re-registering', 1),
+(5, '2020-04-02', 'Re-registering', 1);
+
+INSERT INTO animal_event_check_out (animal, date_time, comments, organization)
+VALUES
+(5, '2020-10-01', 'Adopted cat', 1);
+
+INSERT INTO animal_event_general (animal, date_time, comments, organization, type, expenses)
+VALUES
+(2, '2020-07-01', 'Birthday party!', 1, 'birthday', 109.03);
+
+INSERT INTO animal_event_medical_record (animal, date_time, comments, organization, type, expenses)
+VALUES
+(4, '2020-05-21', 'Dewormed', 1, 'deworm', 26.74);
--- database/schema.sql
+++ database/schema.sql
@@ -1,3 +1,106 @@
-CREATE TABLE test_table (
-    test_var    char(255)
-); 
+CREATE TABLE organization (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(256) NOT NULL,
+    country VARCHAR(512),
+    city VARCHAR(128),
+    street VARCHAR(128),
+    house VARCHAR(32),
+    flat VARCHAR(32),
+    phone VARCHAR(64),
+    mod_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
+
+CREATE EXTENSION moddatetime;
+
+CREATE TRIGGER organization_mod_time
+    BEFORE UPDATE ON organization
+    FOR EACH ROW
+    EXECUTE PROCEDURE moddatetime (mod_time);
+
+CREATE TYPE role_type AS ENUM ('Owner', 'Member', 'Reader', 'Guest');
+
+CREATE TABLE app_user (
+    id VARCHAR(256) PRIMARY KEY,
+    username VARCHAR(128) NOT NULL,
+    name VARCHAR(256),
+    surname VARCHAR(256),
+    email VARCHAR(128),
+    organization INTEGER REFERENCES organization(id),
+    role_type role_type DEFAULT 'Guest',
+    mod_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
+
+CREATE TRIGGER app_user_mod_time
+    BEFORE UPDATE ON app_user
+    FOR EACH ROW
+    EXECUTE PROCEDURE moddatetime (mod_time);
+
+CREATE TYPE status AS ENUM ('healthy', 'vaccinated', 'sick', 'adopted');
+CREATE TYPE gender AS ENUM ('male', 'female');
+CREATE TYPE species AS ENUM ('cat', 'dog', 'hamster', 'parrot', 'snake');
+CREATE TYPE color AS ENUM ('blue', 'red', 'brown', 'grey', 'black', 'white', 'green', 'violet', 'pink', 'sand', 'orange');
+
+CREATE TABLE animal (
+    id SERIAL PRIMARY KEY,
+    organization INTEGER REFERENCES organization(id),
+    registration_no VARCHAR(256) NOT NULL,
+    registration_date DATE DEFAULT CURRENT_DATE NOT NULL,
+    status status,
+    image_url VARCHAR(512),
+    birth_date DATE,
+    name VARCHAR(128),
+    species species,
+    breed VARCHAR(100),
+    gender gender,
+    color color,
+    weight NUMERIC,
+    microchip_id VARCHAR(256),
+    chip_install_date DATE,
+    allergy VARCHAR(128),
+    food VARCHAR(256),
+    comments TEXT,
+    mod_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
+
+CREATE TRIGGER animal_mod_time
+    BEFORE UPDATE ON animal
+    FOR EACH ROW
+    EXECUTE PROCEDURE moddatetime (mod_time);
+
+CREATE TABLE animal_event_check_in (
+    id SERIAL PRIMARY KEY,
+    animal INTEGER REFERENCES animal(id),
+    date_time TIMESTAMP,
+    comments TEXT,
+    organization INTEGER REFERENCES organization(id)
+);
+
+CREATE TABLE animal_event_check_out (
+    id SERIAL PRIMARY KEY,
+    animal INTEGER REFERENCES animal(id),
+    date_time TIMESTAMP,
+    comments TEXT,
+    organization INTEGER REFERENCES organization(id)
+);
+
+CREATE type event_general AS ENUM ('birthday', 'adoption', 'getting petted', 'going for a walk');
+CREATE TABLE animal_event_general (
+    id SERIAL PRIMARY KEY,
+    animal INTEGER REFERENCES animal(id),
+    date_time TIMESTAMP,
+    comments TEXT,
+    organization INTEGER REFERENCES organization(id),
+    type event_general,
+    expenses NUMERIC
+);
+
+CREATE type event_medical AS ENUM ('deworm', 'vaccinate', 'surgery', 'antibiotics');
+CREATE TABLE animal_event_medical_record (
+    id SERIAL PRIMARY KEY,
+    animal INTEGER REFERENCES animal(id),
+    date_time TIMESTAMP,
+    comments TEXT,
+    organization INTEGER REFERENCES organization(id),
+    type event_medical,
+    expenses NUMERIC
+);
