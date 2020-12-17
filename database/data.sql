84b2f2602040d4e624ba1e34c02280c8184bf6fd
AzisK <akrusna@kayak.com>
2020-12-17 23:20:17 +0200

    Improve database #21

--- database/data.sql
+++ database/data.sql
@@ -19,7 +19,7 @@ UPDATE app_user SET email = 'green@mamba.lt' WHERE id = 'dhjbwau74a6';
 
 INSERT INTO animal (organization, registration_no, status, image_url, birth_date, name, species, breed, gender, color, weight, microchip_id, chip_install_date)
 VALUES
-(1, '123Svx', 'vaccinated', 'www.url.lt', '2020-08-01', 'Haskelis', 'dog', 'husky', 'female', 'white', 20, '123', '2020-08-11'),
+(1, '123Svx', 'vaccinated', 'www.url.lt', '2020-08-01', 'Haskelis', 'dog', 'siberian husky', 'female', 'white', 20, '123', '2020-08-11'),
 (2, '321Hi', 'sick', 'www.url.lt', '2020-01-01', 'Jupyteris', 'parrot', NULL, 'female', 'green', 1, '666666', '2020-09-01'),
 (3, '456Carl', 'healthy', 'www.url.lt', '2020-01-01', 'Pitoncas', 'snake', NULL, 'male', 'green', 0.3, '2893402', '2020-03-01'),
 (3, '555Bob', 'adopted', 'www.url.lt', '2020-01-01', 'Javainis', 'hamster', NULL, 'male', 'orange', 0.4, '29387', '2020-04-14'),
--- database/schema.sql
+++ database/schema.sql
@@ -1,5 +1,5 @@
 CREATE TABLE organization (
-    id SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY NOT NULL,
     name VARCHAR(256) NOT NULL,
     country VARCHAR(512),
     city VARCHAR(128),
@@ -20,12 +20,12 @@ CREATE TRIGGER organization_mod_time
 CREATE TYPE role_type AS ENUM ('Owner', 'Member', 'Reader', 'Guest');
 
 CREATE TABLE app_user (
-    id VARCHAR(256) PRIMARY KEY,
+    id VARCHAR(256) PRIMARY KEY NOT NULL,
     username VARCHAR(128) NOT NULL,
     name VARCHAR(256),
     surname VARCHAR(256),
     email VARCHAR(128),
-    organization INTEGER REFERENCES organization(id),
+    organization INTEGER REFERENCES organization(id) NOT NULL,
     role_type role_type DEFAULT 'Guest',
     mod_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
@@ -39,10 +39,14 @@ CREATE TYPE status AS ENUM ('healthy', 'vaccinated', 'sick', 'adopted');
 CREATE TYPE gender AS ENUM ('male', 'female');
 CREATE TYPE species AS ENUM ('cat', 'dog', 'hamster', 'parrot', 'snake');
 CREATE TYPE color AS ENUM ('blue', 'red', 'brown', 'grey', 'black', 'white', 'green', 'violet', 'pink', 'sand', 'orange');
+CREATE TYPE breed AS ENUM (
+    'siberian husky', 'alaskan malamute', 'samoyed', 'greenland dog', 'poodle', 'beagle', 'bulldog', 'chihuahua', 'dachshund', 'french bulldog', 'maltese dog', 'german shepherd', 'great dane', 'greyhound', 'doberman', 'rottweiler', 'pomeranian dog', 'bernese mountain dog', 'shin tzu', 'chow chow', 'basset hound', 'havanese dog', 'irish setter', 'bull terrier', 'staffordshire bull terrier', 'saint bernard', 'afghan hound', 'irish wolfhound', 'pointer', 'american staffordshire terrier', 'bichon frise', 'border collie', 'cavalier king charles spaniel', 'newfoundland dog', 'chinese crested dog', 'pembroke welsh corgi', 'golden retriever', 'labrador retriever', 'lithuanian hound',
+    'ragdoll', 'persian cat', 'maine coon', 'siamese cat', 'sphynx cat', 'british shorthair', 'bengal cat', 'american shorthair', 'birman', 'scottish fold', 'abyssinian cat'
+);
 
 CREATE TABLE animal (
-    id SERIAL PRIMARY KEY,
-    organization INTEGER REFERENCES organization(id),
+    id SERIAL PRIMARY KEY NOT NULL,
+    organization INTEGER REFERENCES organization(id) NOT NULL,
     registration_no VARCHAR(256) NOT NULL,
     registration_date DATE DEFAULT CURRENT_DATE NOT NULL,
     status status,
@@ -50,7 +54,7 @@ CREATE TABLE animal (
     birth_date DATE,
     name VARCHAR(128),
     species species,
-    breed VARCHAR(100),
+    breed breed,
     gender gender,
     color color,
     weight NUMERIC,
@@ -68,39 +72,39 @@ CREATE TRIGGER animal_mod_time
     EXECUTE PROCEDURE moddatetime (mod_time);
 
 CREATE TABLE animal_event_check_in (
-    id SERIAL PRIMARY KEY,
-    animal INTEGER REFERENCES animal(id),
+    id SERIAL PRIMARY KEY NOT NULL,
+    animal INTEGER REFERENCES animal(id) NOT NULL,
     date_time TIMESTAMP,
     comments TEXT,
-    organization INTEGER REFERENCES organization(id)
+    organization INTEGER REFERENCES organization(id) NOT NULL
 );
 
 CREATE TABLE animal_event_check_out (
-    id SERIAL PRIMARY KEY,
-    animal INTEGER REFERENCES animal(id),
+    id SERIAL PRIMARY KEY NOT NULL,
+    animal INTEGER REFERENCES animal(id) NOT NULL,
     date_time TIMESTAMP,
     comments TEXT,
-    organization INTEGER REFERENCES organization(id)
+    organization INTEGER REFERENCES organization(id) NOT NULL
 );
 
 CREATE type event_general AS ENUM ('birthday', 'adoption', 'getting petted', 'going for a walk');
 CREATE TABLE animal_event_general (
-    id SERIAL PRIMARY KEY,
-    animal INTEGER REFERENCES animal(id),
+    id SERIAL PRIMARY KEY NOT NULL,
+    animal INTEGER REFERENCES animal(id) NOT NULL,
     date_time TIMESTAMP,
     comments TEXT,
-    organization INTEGER REFERENCES organization(id),
+    organization INTEGER REFERENCES organization(id) NOT NULL,
     type event_general,
     expenses NUMERIC
 );
 
 CREATE type event_medical AS ENUM ('deworm', 'vaccinate', 'surgery', 'antibiotics');
 CREATE TABLE animal_event_medical_record (
-    id SERIAL PRIMARY KEY,
-    animal INTEGER REFERENCES animal(id),
+    id SERIAL PRIMARY KEY NOT NULL,
+    animal INTEGER REFERENCES animal(id) NOT NULL,
     date_time TIMESTAMP,
     comments TEXT,
-    organization INTEGER REFERENCES organization(id),
+    organization INTEGER REFERENCES organization(id) NOT NULL,
     type event_medical,
     expenses NUMERIC
 );
