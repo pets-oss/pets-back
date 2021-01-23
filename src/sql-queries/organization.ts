831bf0ea3b8a2049340816f62e27f1b27a1d29ad
Ignas Poska <iposka@kayak.com>
2021-01-23 21:00:10 +0200

    Added query to get organization. Added update mutations with validation. Added sql-bricks.

---
+++ src/sql-queries/organization.ts
@@ -0,0 +1,56 @@
+import { QueryConfig } from 'pg';
+import { select, insert, update } from 'sql-bricks-postgres';
+import snakeCaseKeys from 'snakecase-keys';
+
+interface CreateOrganisationInput {
+  name: String;
+  country: String;
+  city: String;
+  streetAddress: String;
+  phone: String;
+}
+
+interface UpdateOrganizationInput {
+  id: number;
+  name: String;
+  country: String;
+  city: String;
+  streetAddress: String;
+  phone: String;
+}
+
+export const getOrganizationQuery = (id: number): QueryConfig =>
+  select().from('organization').where({ id }).toParams();
+
+export const getDeleteTimeQuery = (id: number): QueryConfig =>
+  select('delete_time').from('organization').where({ id }).toParams();
+
+export const getOrganizationsQuery = (): QueryConfig =>
+  select().from('organization').toParams();
+
+export const createOrganizationQuery = (
+  input: CreateOrganisationInput
+): QueryConfig =>
+  insert('organization', snakeCaseKeys(input))
+    .returning(
+      'id, name, country, city, street_address, phone, mod_time, delete_time'
+    )
+    .toParams();
+
+export const updateOrganizationQuery = (
+  input: UpdateOrganizationInput
+): QueryConfig =>
+  update('organization', snakeCaseKeys(input))
+    .where({ id: input.id })
+    .returning(
+      'id, name, country, city, street_address, phone, mod_time, delete_time'
+    )
+    .toParams();
+
+export const deleteOrganizationQuery = (id: number): QueryConfig =>
+  update('organization', { delete_time: 'NOW()' })
+    .where({ id })
+    .returning(
+      'id, name, country, city, street_address, phone, mod_time, delete_time'
+    )
+    .toParams();
