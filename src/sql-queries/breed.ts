9aa4c49a6c9b0eee2b1df85601a5bcd5f43512a1
AzisK <akrusna@kayak.com>
2021-03-04 20:54:44 +0200

    Lint master

--- src/sql-queries/breed.ts
+++ src/sql-queries/breed.ts
@@ -1,13 +1,16 @@
 import { QueryConfig } from 'pg';
 
-export const getBreedsQuery = (species: string, language: string): QueryConfig => {
+export const getBreedsQuery = (
+    species: string,
+    language: string
+): QueryConfig => {
     const text = `
-        SELECT 
+        SELECT
             id,
             abbreviation,
             translation as value
         FROM breed b
-        LEFT JOIN (SELECT * FROM breed_translation WHERE language = $2) bt 
+        LEFT JOIN (SELECT * FROM breed_translation WHERE language = $2) bt
             ON bt.breed = b.id
         WHERE species = $1;
     `;
@@ -18,12 +21,16 @@ export const getBreedsQuery = (species: string, language: string): QueryConfig =
     };
 };
 
-export const getBreedQuery = (breed_id: string, language: string, defaultLanguage: string): QueryConfig => {
+export const getBreedQuery = (
+    breed_id: string,
+    language: string,
+    defaultLanguage: string
+): QueryConfig => {
     const text = `
         SELECT b.id, b.abbreviation, bt.translation as value
         FROM breed b
         LEFT JOIN breed_translation bt ON bt.breed = b.id
-        WHERE bt.breed = $1 
+        WHERE bt.breed = $1
             AND bt.language IN ($2, $3)
         ORDER BY bt.language = $2 DESC
         LIMIT 1;
--- test/interfaces/animalDetails.interface.ts
+++ test/interfaces/animalDetails.interface.ts
@@ -1,14 +1,14 @@
 import Translation from './translation.interface';
-import Breed from "./breed.interface";
+import Breed from './breed.interface';
 
 export default interface AnimalDetails {
-    animalId: number,
-    breed: Breed | null,
-    species: Translation | null,
-    gender: Translation | null,
-    color: Translation | null,
-    birthDate: string | null,
-    weight: number | null,
-    allergy: string | null,
-    food: string | null
+    animalId: number;
+    breed: Breed | null;
+    species: Translation | null;
+    gender: Translation | null;
+    color: Translation | null;
+    birthDate: string | null;
+    weight: number | null;
+    allergy: string | null;
+    food: string | null;
 }
