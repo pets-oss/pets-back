3ac42435d42c7e61e5b9ddf8ca40787de0b13fef
mantasimkus <m.simkus@live.com>
2021-04-08 15:29:33 +0300

    Defined a table of constants outside the SELECT query

--- reports.sql
+++ reports.sql
@@ -1,5 +1,8 @@
 -- Current count of animals in organization grouped by species and gender
 
+WITH const AS (
+    SELECT 'lt' AS lang
+)
 SELECT st.translation AS species,
     gt.translation AS gender,
     COUNT(*) AS cnt
@@ -7,9 +10,15 @@ FROM animal AS a
     JOIN animal_details AS ad ON a.id = ad.animal_id
     JOIN breed AS b ON ad.breed_id = b.id
     JOIN species_translation AS st ON b.species = st.species
-    AND st.language = 'lt'
+    AND st.language = (
+        SELECT lang
+        FROM const
+    )
     JOIN gender_translation AS gt ON ad.gender_id = gt.gender
-    AND gt.language = 'lt'
+    AND gt.language = (
+        SELECT lang
+        FROM const
+    )
 GROUP BY st.translation,
     gt.translation
 ORDER BY st.translation,
