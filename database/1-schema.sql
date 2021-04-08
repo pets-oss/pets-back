4f1334d761764703d268b7186d23ca3edd2fe098
Viktorija Kurauskiene <viktorija.sinkeviciute@gmail.com>
2021-04-08 10:14:16 +0300

    Normalize address in animal_event_found table

--- database/1-schema.sql
+++ database/1-schema.sql
@@ -256,7 +256,8 @@ CREATE TABLE animal_event_medical_record (
 
 CREATE TABLE animal_event_found (
     id SERIAL PRIMARY KEY,
-    address VARCHAR(256),
+    address_street VARCHAR(255) NOT NULL,
+    address_house_number VARCHAR(8),
     municipality_id INTEGER REFERENCES municipality(id) NOT NULL,
     date_time TIMESTAMP,
     animal_id INTEGER REFERENCES animal(id) ON DELETE CASCADE NOT NULL,
