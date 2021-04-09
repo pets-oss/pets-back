32b970023475fd3bf9e30c97fc5f6feaa80fefc3
mantasimkus <m.simkus@live.com>
2021-04-10 00:22:37 +0300

    Adjusted JOIN clauses as suggested by collaborators

--- reports.sql
+++ reports.sql
@@ -7,18 +7,14 @@ SELECT st.translation AS species,
     gt.translation AS gender,
     COUNT(*) AS cnt
 FROM animal AS a
-    JOIN animal_details AS ad ON a.id = ad.animal_id
-    JOIN breed AS b ON ad.breed_id = b.id
-    JOIN species_translation AS st ON b.species = st.species
-    AND st.language = (
-        SELECT lang
-        FROM const
-    )
-    JOIN gender_translation AS gt ON ad.gender_id = gt.gender
-    AND gt.language = (
-        SELECT lang
-        FROM const
-    )
+JOIN animal_details AS ad
+    ON a.id = ad.animal_id
+JOIN breed AS b
+    ON ad.breed_id = b.id
+JOIN (SELECT * FROM species_translation WHERE language = (SELECT lang FROM const)) AS st
+    ON b.species = st.species
+JOIN (SELECT * FROM gender_translation WHERE language = (SELECT lang FROM const)) AS gt
+    ON ad.gender_id = gt.gender
 GROUP BY st.translation,
     gt.translation
 ORDER BY st.translation,
