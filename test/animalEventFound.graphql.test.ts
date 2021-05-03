c9508f2e63fbb79070902b9264afcc5568359662
Viktorija Kurauskiene <viktorija.sinkeviciute@gmail.com>
2021-05-03 22:10:17 +0300

    Add mutation createAnimalFoundEvent tests

--- test/animalEventFound.graphql.test.ts
+++ test/animalEventFound.graphql.test.ts
@@ -1,6 +1,7 @@
 import { expect } from 'chai';
 import supertest from 'supertest';
 import validate from './validators/animalEventFound.interface.validator';
+import { animalFoundEventFields } from './testFields';
 
 require('dotenv').config({ path: './test/.env' });
 
@@ -22,19 +23,95 @@ describe('Animal Event Found', () => {
                       municipalityId
                       comments
                   }
-                }`,
+                }`
             })
             .expect(200)
             .end((err, res) => {
                 if (err) return done(err);
                 const {
                     body: {
-                        data: { foundEvents },
-                    },
+                        data: { foundEvents }
+                    }
                 } = res;
                 expect(foundEvents).to.be.an('array');
                 expect(foundEvents).to.have.length.above(1);
                 validate(foundEvents[0]);
+                return done();
+            });
+    });
+});
+
+describe('Animal Event Found mutations tests', () => {
+    it('Create animal event found', (done) => {
+        const mutation = 'createFoundEvent';
+        const create = `{
+                street: "Gyvūnų gatvė",
+                houseNo: "58",
+                municipalityId: 5,
+                date: "2021-03-19",
+                animalId: 4,
+                comments: "Dog was found dirty and hungry"
+          }`;
+        const answer = {
+            street: 'Gyvūnų gatvė',
+            houseNo: '58',
+            municipalityId: 5,
+            date: '2021-03-19',
+            animalId: 4,
+            comments: 'Dog was found dirty and hungry'
+        };
+
+        request
+            .post('/graphql')
+            .send({
+                query: `
+                      mutation {
+                          ${mutation}(input: ${create})
+                                ${animalFoundEventFields}
+                  }`
+            })
+            .set('Authorization', `Bearer ${process.env.BEARER_TOKEN}`)
+            .expect(200)
+            .end((err, res) => {
+                if (err) {
+                    // eslint-disable-next-line no-console
+                    console.log(res.body);
+                    return done(err);
+                }
+                expect(res.body.data[mutation]).to.deep.include(answer);
+                return done();
+            });
+    });
+    it('Create animal event found with invalid input throws Error', (done) => {
+        const mutation = 'createFoundEvent';
+        const create = `{
+                street: "Gyvūnų gatvė",
+                houseNo: "6",
+                municipalityId: 5,
+                date: "2040-03-19",
+                animalId: 4,
+                comments: "Dog was found dirty and hungry"
+          }`;
+
+        request
+            .post('/graphql')
+            .send({
+                query: `
+                      mutation {
+                          ${mutation}(input: ${create})
+                            ${animalFoundEventFields}
+                  }`
+            })
+            .set('Authorization', `Bearer ${process.env.BEARER_TOKEN}`)
+            .expect(200)
+            .end((err, res) => {
+                if (err) {
+                    // eslint-disable-next-line no-console
+                    console.log(res.body);
+                    return done(err);
+                }
+                expect(res.body.data[mutation]).equal(null);
+                expect(res.body.errors[0].message).to.have.include('date');
                 return done();
             });
     });
