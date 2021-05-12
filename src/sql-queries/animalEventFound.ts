a532c0d8ae7d5f27e0ab05f83b9fea05c6e2c96b
Viktorija Kurauskiene <48152843+pergaliuke@users.noreply.github.com>
2021-05-12 18:13:10 +0300

    Feature/#112 create mutation for animal found event (#223)
    * Add mutation createAnimalFoundEvent with validation
    
    * Add mutation createAnimalFoundEvent tests
    
    * Change thrown Error to ValidationError

--- src/sql-queries/animalEventFound.ts
+++ src/sql-queries/animalEventFound.ts
@@ -1,20 +1,42 @@
 import { QueryConfig } from 'pg';
+import { insert } from 'sql-bricks-postgres';
+import snakeCaseKeys from 'snakecase-keys';
 
-const getAnimalFoundEventsQuery = (): QueryConfig => {
+const table = 'animal_event_found';
+const returningFields = 'id, street, house_no, municipality_id, date_time AS date, animal_id, comments';
+
+export const getAnimalFoundEventsQuery = (): QueryConfig => {
     const text = `
-        SELECT
-            id,
-            street,
-            house_no,
-            municipality_id,
-            date_time AS date,
-            animal_id,
-            comments
+        SELECT id,
+               street,
+               house_no,
+               municipality_id,
+               date_time AS date,
+               animal_id,
+               comments
         FROM animal_event_found;`;
 
     return {
-        text,
+        text
     };
 };
 
-export default getAnimalFoundEventsQuery;
+interface CreateAnimalFoundEventInput {
+    street: String
+    houseNo: String
+    municipalityId: number
+    date: String
+    animalId: number
+    comments: String
+}
+
+function dateToDateTime(input: CreateAnimalFoundEventInput) {
+    const {date, ...otherFields} = input;
+    return { dateTime: date, ...otherFields };
+}
+
+export const createAnimalFoundEventQuery =
+    (input: CreateAnimalFoundEventInput): QueryConfig =>
+        insert(table, snakeCaseKeys(dateToDateTime(input)))
+            .returning(returningFields).toParams();
+
