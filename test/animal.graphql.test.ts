c2652c13badc05234ccfeea04fd6cd0347fc63dd
mantasimkus <m.simkus@live.com>
2021-05-15 21:11:45 +0300

    Added filters for querying animals by species, gender and breed ids

--- test/animal.graphql.test.ts
+++ test/animal.graphql.test.ts
@@ -85,6 +85,36 @@ describe('GraphQL animal integration tests', () => {
                 return done();
             });
     });
+
+    it('Returns all animals with all fields filtered by species ids [1, 2], gender ids [1, 2] and breed ids [205, 268, 350]', (done) => {
+        request
+            .post('/graphql')
+            .send({
+                query: `
+                {
+                    animals (species: [1, 2], gender: [1, 2], breed: [205, 268, 350])
+                        ${animalFields}
+                }`,
+            })
+            .set('Authorization', `Bearer ${process.env.BEARER_TOKEN}`)
+            .expect(200)
+            .end((err, res) => {
+                if (err) {
+                    // eslint-disable-next-line no-console
+                    console.log(res.body);
+                    return done(err);
+                }
+                const {
+                    body: {
+                        data: { animals },
+                    },
+                } = res;
+                expect(animals).to.be.an('array');
+                validate(animals[0]);
+                expect(animals).to.have.length(3);
+                return done();
+            });
+    });
 });
 
 describe('animal mutations tests', () => {
