20f81bef7f3ce7add40aa44f0927a5787d47ebdc
Viktorija Kurauskiene <viktorija.sinkeviciute@gmail.com>
2021-05-20 11:28:35 +0300

    Extract country Code to config file

--- src/schema/scalars/scalars.ts
+++ src/schema/scalars/scalars.ts
@@ -1,9 +1,10 @@
 import getStringScalar from './getStringScalarWithValidation';
+import config from '../../config';
 
 const nameScalar = getStringScalar({ name: 'Name', max: 255 });
 const surnameScalar = getStringScalar({ name: 'Surname', max: 255 });
 const phoneScalar = getStringScalar({
-    name: 'Phone', pattern: /^\+3706\d{7}$/
+    name: 'Phone', pattern: new RegExp(`^\\+${config.phoneCountryCode}\\d{8,10}$`)
 });
 
 export { nameScalar, surnameScalar, phoneScalar };
