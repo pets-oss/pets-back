39c424577f30162d61cd289cff4b21d5953c295c
Viktorija Kurauskiene <viktorija.sinkeviciute@gmail.com>
2021-05-25 09:57:21 +0300

    Add null check on update former animal owner input

--- src/schema/resolvers/formerAnimalOwner.ts
+++ src/schema/resolvers/formerAnimalOwner.ts
@@ -1,4 +1,5 @@
 import { IResolvers } from 'graphql-tools';
+import { ValidationError } from 'apollo-server-express';
 import {
     getFormerAnimalOwnersQuery,
     getFormerAnimalOwnerQuery,
@@ -28,7 +29,20 @@ const resolvers: IResolvers = {
             );
             return dbResponse.rows[0];
         },
-        updateFormerAnimalOwner: async (_, { input }, { pgClient }) => {
+        updateFormerAnimalOwner: async (
+            _,
+            { input },
+            { pgClient }
+        ) => {
+            if (Object.keys(input).length < 2) {
+                throw new ValidationError(
+                    'You have to provide at least one data field when updating an entity'
+                );
+            }
+            if (input.name === null) {
+                throw new ValidationError('Name can not be null');
+            }
+
             const dbResponse = await pgClient.query(
                 updateFormerAnimalOwnerQuery(input)
             );
