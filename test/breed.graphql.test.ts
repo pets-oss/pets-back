f9ebe42d606370acd4eaf15101631cf32edddc0d
Viktorija Kurauskiene <viktorija.sinkeviciute@gmail.com>
2021-11-23 12:55:14 +0200

    Use request util in tests

--- test/breed.graphql.test.ts
+++ test/breed.graphql.test.ts
@@ -1,11 +1,6 @@
 import { expect } from 'chai';
-import supertest from 'supertest';
 import validate from './validators/breedWithSpecies.interface.validator';
-
-require('dotenv').config({ path: './test/.env' });
-
-const url = process.env.TEST_URL || 'http://localhost:8081';
-const request = supertest(url);
+import requests from './utils/requests';
 
 const breedFields = `
     {
@@ -19,20 +14,13 @@ const breedFields = `
 
 describe('GraphQL breed integration tests', () => {
     it('Returns breeds translation in "lt" with all fields filtered by species ', (done) => {
-        let req = request
-            .post('/graphql')
-            .send({
-                query: `{ breeds(species: "2", language: "lt")
+        const req = requests(`{ breeds(species: "2", language: "lt")
                     ${breedFields}
-                }`,
-            });
-        if (process.env.BEARER_TOKEN) {
-            req = req.set('authorization', `Bearer ${process.env.BEARER_TOKEN}`)
-        }
+                }`);
         req.expect(200)
             .end((err, res) => {
                 if (err) return done(err);
-                const { body: {data: { breeds } } } = res;
+                const { body: { data: { breeds } } } = res;
                 expect(breeds).to.be.an('array');
                 validate(breeds[0]);
                 return done();
@@ -40,20 +28,13 @@ describe('GraphQL breed integration tests', () => {
     });
 
     it('Returns all breeds translation in "lt" with all fields', (done) => {
-        let req = request
-            .post('/graphql')
-            .send({
-                query: `{ breeds(language: "lt")
+        const req = requests(`{ breeds(language: "lt")
                     ${breedFields}
-                }`,
-            });
-        if (process.env.BEARER_TOKEN) {
-            req = req.set('authorization', `Bearer ${process.env.BEARER_TOKEN}`)
-        }
+                }`);
         req.expect(200)
             .end((err, res) => {
                 if (err) return done(err);
-                const { body: {data: { breeds } } } = res;
+                const { body: { data: { breeds } } } = res;
                 expect(breeds).to.be.an('array');
                 validate(breeds[0]);
                 return done();
